74f50f311bf71db1a0bae23de393af47d89d525a
Vinicius S N <88517353+Sacimia@users.noreply.github.com>
2021-09-03 18:25:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import {React} from 'react'
+import { ReactDOM } from 'react-dom'
+
+import {Botoes} from './componentes/botoes'
+ReactDOM.render(
+        <Botoes/>
+    , document.getElementById('root'))
